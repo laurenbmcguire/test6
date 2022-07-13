c23f5aafd95c138dbb106658dcd12da3b95b465f
laurenbmcguire <102077139+laurenbmcguire@users.noreply.github.com>
2022-07-13 15:21:53 -0500

    Add files via upload

---
+++ Bo$$App/src/components/Bo$$Money.js
@@ -0,0 +1,15 @@
+
+    import React, { Component } from 'react'
+
+    class Bo$$Money extends Component {
+        render() {
+            return (
+                <div>
+                    <h1>Bo$$Money</h1>
+                </div>
+            )
+        }
+    }
+    
+    export default Bo$$Money
+    
